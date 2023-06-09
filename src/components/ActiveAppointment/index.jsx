0ed19ee3331f7f2f8a561bf1fcd91fedaf2eb756
Gaurav-S-Rao <gauravsrao.dev@gmail.com>
2023-06-10 02:06:22 +0530

    added sheet db for creating and viewing of appointments

--- src/components/ActiveAppointment/index.jsx
+++ src/components/ActiveAppointment/index.jsx
@@ -1,7 +1,33 @@
-import { Container, Toolbar, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  Paper,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 function ActiveAppointment() {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      await fetch("https://sheetdb.io/api/v1/vmdyjsot299jv")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          setAppointments(data);
+        });
+    }
+    fetchData();
+  }, []);
+
   return (
     <>
       <Toolbar>
@@ -22,7 +48,50 @@ function ActiveAppointment() {
           m: "10px",
         }}
       >
-        <h1>View Active Appointment</h1>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>USER ID</TableCell>
+                <TableCell align="right">NAME</TableCell>
+                <TableCell align="right">Contact Number</TableCell>
+                <TableCell align="right">Purpose Of Visit</TableCell>
+                <TableCell align="right">Appointment Date</TableCell>
+              </TableRow>
+            </TableHead>
+          </Table>
+        </TableContainer>
+        {appointments.length === 0 ? (
+          <Typography variant="h5" component="div">
+            No Active Appointment
+          </Typography>
+        ) : (
+          appointments.map((appointment) => (
+            <>
+              <TableRow
+                key={appointment}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {appointment?.userId}
+                </TableCell>
+                <TableCell align="right">{appointment.name}</TableCell>
+                <TableCell align="right">{appointment.emailId}</TableCell>
+                <TableCell align="right">{appointment.number}</TableCell>
+                <TableCell align="right">
+                  {appointment.purposeOfVisit}
+                </TableCell>
+                <TableCell align="right">
+                  {appointment.appointmentdate}
+                </TableCell>
+                {/* show a tick icon or wrong based on TRUE or FALSE */}
+                <TableCell align="right">
+                  {appointment.approvalByHost}
+                </TableCell>
+              </TableRow>
+            </>
+          ))
+        )}
       </Container>
     </>
   );
--- src/components/CreateAppointment/index.jsx
+++ src/components/CreateAppointment/index.jsx
@@ -1,11 +1,48 @@
-import { Container, Toolbar, Typography } from "@mui/material";
+import { Box, Container, Toolbar, Typography } from "@mui/material";
+import { v4 as uuidv4 } from "uuid";
 import PropTypes from "prop-types";
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
+import { DateTimePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { useState } from "react";
+import dayjs from "dayjs";
 
 function CreateAppointment() {
+  const [appointmentdate, setAppointmentdate] = useState(dayjs());
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const prepareData = {
+      userId: uuidv4(),
+      name: data.get("name"),
+      emailId: data.get("email"),
+      number: data.get("number"),
+      purposeOfVisit: data.get("purposeOfVisit"),
+      appointmentdate: dayjs(appointmentdate).format("DD/MM/YYYY HH:mm:ss"),
+      approvalByHost: "FALSE",
+    };
+    await fetch("https://sheetdb.io/api/v1/vmdyjsot299jv", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        data: [prepareData],
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+      
+      Array.from(document.querySelectorAll("input")).forEach(
+        input => (input.value = "")
+      );
+  };
+
   return (
-    <>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Toolbar>
         <Typography variant="h4" component="div">
           Creation of Appointment
@@ -24,73 +61,64 @@ function CreateAppointment() {
           m: "10px",
         }}
       >
-        <div className="hero1-wrap flex flex-col gap-10 ">
-          <div className="left-container flex flex-col justify-center items-center gap-5">
-            <div className="items-center">
-              {" "}
-              Name:{" "}
-              <TextField
-                sx={{
-                  m: 1,
-                }}
-                id="outlined-basic"
-                label="enter name"
-                variant="outlined"
-              />{" "}
-            </div>
-            <div className="items-center">
-              {" "}
-              Email:{" "}
-              <TextField
-                sx={{
-                  m: 1,
-                }}
-                id="outlined-basic"
-                label="enter email"
-                variant="outlined"
-              />
-            </div>
-            <div className="items-center">
-              {" "}
-              Phone:{" "}
-              <TextField
-                sx={{
-                  m: 1,
-                }}
-                id="outlined-basic"
-                label="enter phone no."
-                variant="outlined"
-              />
-            </div>
-            <div className="items-center">
-              {" "}
-              Purpose:{" "}
-              <TextField
-                sx={{
-                  m: 1,
-                }}
-                id="outlined-basic"
-                label="enter purpose of visit"
-                variant="outlined"
-              />
-            </div>
-            <div className="items-center">
-              {" "}
-              Date:{" "}
-              <TextField
-                sx={{
-                  m: 1,
-                }}
-                id="outlined-basic"
-                label="enter date of visit"
-                variant="outlined"
-              />
-            </div>
-          </div>
-          <Button variant="contained">Submit</Button>
-        </div>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          noValidate
+          sx={{ mt: 1, display: "flex", flexDirection: "column" }}
+        >
+          <TextField
+            margin="normal"
+            required
+            id="name"
+            label="User Name"
+            name="name"
+            autoComplete="name"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+          />
+          <TextField
+            margin="normal"
+            required
+            name="number"
+            label="Contact Number"
+            type="number"
+            id="number"
+          />
+          <TextField
+            margin="normal"
+            required
+            id="purposeOfVisit"
+            label="Purpose of Visit"
+            name="purposeOfVisit"
+            autoComplete="purposeOfVisit"
+            autoFocus
+          />
+
+          <DateTimePicker
+            label="Set Appointment"
+            value={appointmentdate}
+            onChange={(newValue) => setAppointmentdate(newValue)}
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Create Appointment
+          </Button>
+        </Box>
       </Container>
-    </>
+    </LocalizationProvider>
   );
 }
 CreateAppointment.propTypes = {
