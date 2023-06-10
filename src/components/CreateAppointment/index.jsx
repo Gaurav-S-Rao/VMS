ec332da929291b4057fe9db74a2527dcfad0e039
Gaurav-S-Rao <gauravsrao.dev@gmail.com>
2023-06-10 13:24:10 +0530

    qr code generation

--- src/components/CreateAppointment/index.jsx
+++ src/components/CreateAppointment/index.jsx
@@ -10,16 +10,19 @@ import { useState } from "react";
 import dayjs from "dayjs";
 import { useNotification } from "../../hooks/useNotification";
 import emailjs from "@emailjs/browser";
+import QRCode from "react-qr-code";
 
 function CreateAppointment() {
   const [appointmentdate, setAppointmentdate] = useState(dayjs());
+  const [loggedIn, setLoggedIn] = useState(0);
   const { displayNotification } = useNotification();
+  const userIdHere = uuidv4();
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
 
     const prepareData = {
-      userId: uuidv4(),
+      userId: userIdHere,
       name: data.get("name"),
       emailId: data.get("email"),
       number: data.get("number"),
@@ -27,6 +30,8 @@ function CreateAppointment() {
       appointmentdate: dayjs(appointmentdate).format("DD/MM/YYYY HH:mm:ss"),
       approvalByHost: "FALSE",
     };
+
+    console.log(prepareData);
 
     await fetch("https://sheetdb.io/api/v1/vmdyjsot299jv", {
       method: "POST",
@@ -39,7 +44,10 @@ function CreateAppointment() {
       }),
     })
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        setLoggedIn(data?.created);
+      });
 
     emailjs.send(
       "service_uwbg1rs",
@@ -78,62 +86,88 @@ function CreateAppointment() {
           m: "10px",
         }}
       >
-        <Box
-          component="form"
-          onSubmit={handleSubmit}
-          noValidate
-          sx={{ mt: 1, display: "flex", flexDirection: "column" }}
-        >
-          <TextField
-            margin="normal"
-            required
-            id="name"
-            label="User Name"
-            name="name"
-            autoComplete="name"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-          />
-          <TextField
-            margin="normal"
-            required
-            name="number"
-            label="Contact Number"
-            type="number"
-            id="number"
-          />
-          <TextField
-            margin="normal"
-            required
-            id="purposeOfVisit"
-            label="Purpose of Visit"
-            name="purposeOfVisit"
-            autoComplete="purposeOfVisit"
-            autoFocus
-          />
-
-          <DateTimePicker
-            label="Set Appointment"
-            value={appointmentdate}
-            onChange={(newValue) => setAppointmentdate(newValue)}
-          />
-
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+        {loggedIn === 0 ? (
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1, display: "flex", flexDirection: "column" }}
           >
-            Create Appointment
-          </Button>
-        </Box>
+            <TextField
+              margin="normal"
+              required
+              id="name"
+              label="User Name"
+              name="name"
+              autoComplete="name"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+            />
+            <TextField
+              margin="normal"
+              required
+              name="number"
+              label="Contact Number"
+              type="number"
+              id="number"
+            />
+            <TextField
+              margin="normal"
+              required
+              id="purposeOfVisit"
+              label="Purpose of Visit"
+              name="purposeOfVisit"
+              autoComplete="purposeOfVisit"
+              autoFocus
+            />
+
+            <DateTimePicker
+              label="Set Appointment"
+              value={appointmentdate}
+              onChange={(newValue) => setAppointmentdate(newValue)}
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Create Appointment
+            </Button>
+          </Box>
+        ) : (
+          <div
+            style={{
+              height: "auto",
+              margin: "0 auto",
+              maxWidth: 256,
+              width: "100%",
+            }}
+          >
+            <Typography
+              variant="primary"
+              sx={{
+                fontSize: "50px",
+              }}
+            >
+              Qr Code here
+            </Typography>
+            <QRCode
+              size={256}
+              style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+              value={userIdHere}
+              viewBox={`0 0 256 256`}
+            />
+          </div>
+        )}
       </Container>
     </LocalizationProvider>
   );
